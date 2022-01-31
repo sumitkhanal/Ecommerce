823771ca5a2be68dc15ac09ae6bf5ec48b43b79e
sumitkhanal <sumit.khanal80@gmail.com>
2022-01-31 10:57:59 +0545

    files in backend added

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,7 @@ const express = require("express");
 
 const app = express();
 
-// app.use(
-//     express.urlencoded({ extended: true })
-// );
+
 
 const errorMiddleware = require("./middleware/error")
 
@@ -16,9 +14,12 @@ app.use(cookieParser());
 //Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 app.use("/api/v1",product);
 app.use("/api/v1",user);
+app.use("/api/v1",order);
+
 //Middleware for Errors
 
 app.use(errorMiddleware);
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser, loginUser,forgotPassword, logout, resetPassword} = require("../controllers/userController");
+const {registerUser, loginUser,forgotPassword, logout, resetPassword,getUserDetails,updatePassword,updateProfile,getAllUser,getSingleUser,updateUserRole,deleteUser} = require("../controllers/userController");
+const { isAuthenticatedUser, authorizeRoles} = require("../middleware/auth");
+
 
 router.route("/register").post(registerUser);
 
@@ -11,5 +13,21 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
+
+  router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
